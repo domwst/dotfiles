208091c7b350637540aa05c2970b350a84ef8681
Oleg Shatov <8730199@gmail.com>
2024-05-09 10:09:34 +0200

    Upd

--- tmux/switch_theme.sh
+++ tmux/switch_theme.sh
@@ -4,11 +4,17 @@ TMUX_CONF=$(readlink -f "$HOME/.tmux.conf")
 
 PLACEHOLDER="SOME_RANDOM_STRING"
 
+if [[ $(uname) = "Linux" ]]; then
+  SED="sed -i"
+else
+  SED="sed -i ''"
+fi
+
 THEME1="set -g @catppuccin_flavour 'latte'"
 THEME2="set -g @catppuccin_flavour 'mocha'"
 
-sed -i '' "s/$THEME1/$PLACEHOLDER/g" $TMUX_CONF
-sed -i '' "s/$THEME2/$THEME1/g" $TMUX_CONF
-sed -i '' "s/$PLACEHOLDER/$THEME2/g" $TMUX_CONF
+$SED "s/$THEME1/$PLACEHOLDER/g" $TMUX_CONF
+$SED "s/$THEME2/$THEME1/g" $TMUX_CONF
+$SED "s/$PLACEHOLDER/$THEME2/g" $TMUX_CONF
 
 tmux source-file $TMUX_CONF
