3a85bbf11ef2c1d5d90912f9de355d22bc2c2da4
Oleg Shatov <oshatov@cerera.one>
2024-10-01 10:48:11 +0300

    Disable cmake autoformatting

--- nvim/init.lua
+++ nvim/init.lua
@@ -823,7 +823,7 @@ require('lazy').setup({
         -- Disable "format_on_save lsp_fallback" for languages that don't
         -- have a well standardized coding style. You can add additional
         -- languages here or re-enable it for the disabled ones.
-        local disable_filetypes = { c = true, cpp = true, python = true }
+        local disable_filetypes = { c = true, cpp = true, python = true, cmake = true }
         return {
           timeout_ms = 500,
           lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
