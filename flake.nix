050401568e97ab465a94e323631befa47aac7d68
Oleg Shatov <oshatov@cerera.one>
2026-04-14 11:16:00 +0300

    Vendor rust-analyzer

--- flake.nix
+++ flake.nix
@@ -57,7 +57,7 @@
           (
             rust-bin.selectLatestNightlyWith (toolchain:
               toolchain.default.override {
-                extensions = ["rust-src" "rustc-dev" "miri" "llvm-tools-preview"];
+                extensions = ["rust-src" "rustc-dev" "miri" "llvm-tools-preview" "rust-analyzer"];
                 targets = ["wasm32-unknown-unknown"];
               })
           )
