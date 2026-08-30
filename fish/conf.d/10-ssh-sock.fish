eedf8bba917befacf5e6573139f4334f0258b78a
Oleg Shatov <8730199@gmail.com>
2026-08-30 19:03:41 +0200

    Don't link sock if no remote agent is present

--- fish/conf.d/10-ssh-sock.fish
+++ fish/conf.d/10-ssh-sock.fish
@@ -4,7 +4,7 @@ set -q SSH_TTY; or return
 set -q SSH_CLIENT; or return
 
 set -l AUTH_SOCK "$HOME/.ssh/auth.sock"
-if test "$SSH_AUTH_SOCK" != "$AUTH_SOCK"
+if test "$SSH_AUTH_SOCK" != "$AUTH_SOCK"; and test "$SSH_AUTH_SOCK" != ""
     ln -sf "$SSH_AUTH_SOCK" "$AUTH_SOCK"
     set -gx SSH_AUTH_SOCK "$AUTH_SOCK"
 end
