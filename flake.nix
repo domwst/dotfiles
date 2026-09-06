76bd19c186855eb5365b39f7619d54daf8d2cd64
Oleg Shatov <8730199@gmail.com>
2026-09-06 13:59:48 +0200

    Remove unused overlay, bump flake.lock

--- flake.nix
+++ flake.nix
@@ -5,8 +5,6 @@
     nixpkgs = {url = "github:NixOS/nixpkgs/master";};
     # nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
 
-    nixpkgs-codex = {url = "github:m0nhawk/nixpkgs/036633397494a1c942be04c98e364b37336e5d29";};
-
     nix-darwin = {
       url = "github:nix-darwin/nix-darwin/master";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -26,7 +24,6 @@
   outputs = {
     self,
     nixpkgs,
-    nixpkgs-codex,
     nix-darwin,
     home-manager,
     rust-overlay,
@@ -38,9 +35,6 @@
         inherit system;
         overlays = [
           rust-overlay.overlays.default
-          (final: prev: {
-            codex = (import nixpkgs-codex {inherit system;}).codex;
-          })
         ];
       };
 
